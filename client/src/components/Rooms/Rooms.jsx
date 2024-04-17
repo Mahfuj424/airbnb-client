8ea42024adc7188b4b7cd449d628a490ca90ec9f
mahfuj <mahfujahmad424@gmail.com>
2024-04-17 10:35:52 +0600

    nothing

--- client/src/components/Rooms/Rooms.jsx
+++ client/src/components/Rooms/Rooms.jsx
@@ -34,7 +34,7 @@ const Rooms = () => {
   return (
     <Container>
       {rooms && rooms.length > 0 ? (
-        <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
+        <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-5 gap-8'>
           {rooms.map((room, index) => (
             <Card key={index} room={room} />
           ))}
--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -29,9 +29,7 @@ const SignUp = () => {
     const formData = new FormData()
     formData.append('image', image)
 
-    const url = `https://api.imgbb.com/1/upload?key=${
-      import.meta.env.VITE_IMGBB_KEY
-    }`
+    const url = `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_KEY}`
     fetch(url, {
       method: 'POST',
       body: formData,
